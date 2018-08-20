1e68561f9e6c0a21574e9cf8912964ab21320ce8
ZhengChen He <31530891+HHeOC@users.noreply.github.com>
2018-08-20 11:56:40 -1000

    Add files via upload

--- foodchoose.js
+++ foodchoose.js
@@ -1,12 +1,8 @@
-var adj = [
-"long",
-"short",
-"big",
-"little",
-"large",
-"small"
-];
-
-var username = prompt("what is your name?");
-var description = [Math.floor(Math.random()*adj.length)];
-window.alert(username + " is " + adj[description]);
+var foods = [
+  "Sushi",
+  "taco",
+  "Steak"
+  ];
+  
+  var choose = [Math.floor(Math.random()*foods.length)]
+  window.alert(foods[choose])
